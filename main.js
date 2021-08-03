a105e862c5063e203eae8eae944ccac32d19fbf1
Andrius <andriuss0316@gmail.com>
2021-08-03 12:53:08 +0300

    20210803

--- main.js
+++ main.js
@@ -7,9 +7,13 @@ const plusDOM = document.querySelector(".plus");
 const numberDOM = document.querySelector(".number");
 const resetDOM = document.querySelector(".reset");
 
+const h1ND8 = document.querySelector("h1");
+
 function minusClick() {
     rezultatas = rezultatas - 1;
     numberDOM.innerText = rezultatas;
+
+    h1ND8.innerText = "Žaidimas progrese";
 
     // galima buvo rasyti = numberDOM.innerText = --rezultatas
 }
@@ -18,12 +22,16 @@ function plusCick() {
     rezultatas = rezultatas + 1;
     numberDOM.innerText = rezultatas;
 
+    h1ND8.innerText = "Žaidimas progrese";
+
     // galima buvo rasyti = numberDOM.innerText = ++rezultatas
 }
 
 function resetClick() {
     rezultatas = 0;
     numberDOM.innerText = rezultatas;
+
+    h1ND8.innerText = "Show must go on!";
 }
 
 minusDOM.addEventListener('click', minusClick);
